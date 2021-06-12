1e415c5fb09ed22cd286c3e24383dc3c62b582a8
EC2 Default User <ec2-user@ip-172-31-14-137.sa-east-1.compute.internal>
2021-06-12 22:00:30 +0000

    master added dynamo better version

--- dynamo_api/src/user.py
+++ dynamo_api/src/user.py
@@ -9,27 +9,45 @@ table = dynamodb.Table(users_table)
 def getUser(event, context):
     print(json.dumps({"running": True}))
     print(json.dumps(event))
+    
+    path = event["path"]    #user/123
+    array_path = path.split("/") ##[user,123]
+    user_id = array_path[-1]
+    
     response = table.get_item(
-    Key={
-        'pk': 'user_',
-        'sk': 'age'
+        Key={
+            'pk': user_id,
+            'sk': 'age'
         }
     )
     item = response['Item']
     print(item)
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps(item)
     }
 
 def putUser(event, context):
     print(json.dumps({"running": True}))
     print(json.dumps(event))
     
+    path = event["path"]    #user/123
+    print("Imprimiendo paht:",path)
+    array_path = path.split("/") ##[user,123]
+    users_id = array_path[-1]
+    
+    body = event["body"]   
+    body_obj = json.loads(body)
+    
+    print("Imprimiendo paht:",users_id)
+    
     table.put_item(
         Item={
-        'pk': 'user_',
-        'sk': 'age'
+            'pk': users_id,
+            'sk': 'age',
+            'name': body_obj['name'],
+            'last_name': body_obj['last_name'],
+            'age': body_obj['age']
        
         }
     )
